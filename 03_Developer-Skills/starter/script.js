de92cf7e8383574e5a793a5020e24c95344689e7
Nelief <madima.mauriade@gmail.com>
2023-11-03 12:56:33 +0100

    update

--- 03_Developer-Skills/starter/script.js
+++ 03_Developer-Skills/starter/script.js
@@ -1,4 +1,46 @@
-// Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-console.log();
+/* const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calculateAmplitude = function (temperatures) {
+  let max = temperatures[0];
+  let min = temperatures[0];
+  for (let i = 1; i < temperatures.length; i++) {
+    if (typeof temperatures[i] !== 'number') continue;
+    if (temperatures[i] > max) max = temperatures[i];
+    if (temperatures[i] < min) min = temperatures[i];
+    console.log(min, max);
+  }
+
+  return max - min;
+};
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: Number(prompt('degree celsius')),
+  };
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+ */
+
+const printForecast = function (forecastArray) {
+  let forecastString = '';
+  let tempString = '';
+  for (let i = 0; i < forecastArray.length; i++) {
+    tempString = `...${forecastArray[i]}°C in ${i + 1} days `;
+    forecastString = forecastString + tempString;
+  }
+  return forecastString;
+};
+
+const forecastArray1 = [17, 21, 23];
+const forecastArray2 = [12, 5, -5, 0, 4];
+
+console.log(printForecast(forecastArray1));
+console.log(printForecast(forecastArray2));

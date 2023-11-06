7761af3326fc8eca955c364d81b8f2741d799f0f
Nelief <madima.mauriade@gmail.com>
2023-11-06 16:12:04 +0100

    dice game

--- 07_PigGame/starter/script.js
+++ 07_PigGame/starter/script.js
@@ -27,11 +27,29 @@ totalScoresElements[0].textContent = 0;
 totalScoresElements[1].textContent = 0;
 diceElement.classList.add('hidden');
 
-let win = false;
-let randomNumber = 0;
-let activePlayer = 0;
-let scores = [0, 0];
-let totalScores = [0, 0];
+const WIN_CONDITION = 20;
+let win;
+let randomNumber;
+let activePlayer;
+let scores = [];
+let totalScores = [];
+
+const initGame = function () {
+  for (let i = 0; i < 2; i++) {
+    totalScores[i] = 0;
+    scores[i] = 0;
+    totalScoresElements[i].textContent = 0;
+    scoresElements[i].textContent = 0;
+    playerAreaElement[i].classList.remove('player--winner');
+  }
+  win = false;
+  randomNumber = 0;
+  activePlayer = 0;
+  playerAreaElement[1].classList.remove('player--active');
+  playerAreaElement[0].classList.add('player--active');
+};
+
+initGame();
 
 btnRollDice.addEventListener('click', function () {
   if (!win) {
@@ -46,14 +64,31 @@ btnRollDice.addEventListener('click', function () {
       //Add the dice roll to the score
       scores[activePlayer] += randomNumber;
       scoresElements[activePlayer].textContent = scores[activePlayer];
-
-      console.log(`Player ${activePlayer + 1} | Score ${scores[activePlayer]}`);
     } else {
       //reset local score and swap player
       resetScore(activePlayer);
       swapPlayer();
     }
   }
+});
+
+//hold the local score, add the local score to total score for the active player, then reset local score and swap player. if total score for the holding player is >100, then he wins the game. (win = true blocks players from rolling dices until the game is restarted)
+btnHold.addEventListener('click', function () {
+  if (!win) {
+    totalScores[activePlayer] += scores[activePlayer];
+    if (totalScores[activePlayer] >= WIN_CONDITION) {
+      winGame(activePlayer);
+    } else {
+      totalScoresElements[activePlayer].textContent = totalScores[activePlayer];
+      resetScore(activePlayer);
+      swapPlayer();
+    }
+  }
+});
+
+//reset the game, resets both local and total scores, set active player to 0 and win to false
+btnNewGame.addEventListener('click', function () {
+  initGame();
 });
 
 //swaps the active player
@@ -70,32 +105,9 @@ const resetScore = function (activePlayer) {
   scoresElements[activePlayer].textContent = scores[activePlayer];
 };
 
-//hold the local score, add the local score to total score for the active player, then reset local score and swap player. if total score for the holding player is >100, then he wins the game. (win = true blocks players from rolling dices until the game is restarted)
-btnHold.addEventListener('click', function () {
-  if (!win) {
-    totalScores[activePlayer] += scores[activePlayer];
-
-    if (totalScores[activePlayer] >= 100) {
-      win = true;
-      totalScoresElements[activePlayer].textContent = 'WIN';
-    } else {
-      totalScoresElements[activePlayer].textContent = totalScores[activePlayer];
-      resetScore(activePlayer);
-      swapPlayer();
-    }
-  }
-});
-
-//reset the game, resets both local and total scores, set active player to 0 and win to false
-btnNewGame.addEventListener('click', function () {
-  for (let i = 0; i < 2; i++) {
-    totalScores[i] = 0;
-    scores[i] = 0;
-    totalScoresElements[i].textContent = 0;
-    scoresElements[i].textContent = 0;
-  }
-  win = false;
-  activePlayer = 0;
-  playerAreaElement[0].classList.add('player--active');
-  playerAreaElement[1].classList.remove('player--active');
-});
+const winGame = function (activeplayer) {
+  win = true;
+  totalScoresElements[activePlayer].textContent = 'WIN';
+  playerAreaElement[activePlayer].classList.remove('player--active');
+  playerAreaElement[activePlayer].classList.add('player--winner');
+};
